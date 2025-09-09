476d7e43afbe1ef9cb449b7fef214fc3e82dda33
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2025-09-10 00:42:32 +0600

    product has been created

--- client/src/routes/admin/index.tsx
+++ client/src/routes/admin/index.tsx
@@ -9,6 +9,9 @@ import ViewUser from "@/view/admin/users/view";
 import AllSupplier from "@/view/admin/supplier/all";
 import SupplierForm from "@/view/admin/supplier/form";
 import SupplierDetails from "@/view/admin/supplier/view";
+import AllProducts from "@/view/admin/product/all/intex";
+import ProductForm from "@/view/admin/product/form";
+import ProductDetails from "@/view/admin/product/view";
 
 const adminRoutes = [
     { path: "/unauthorized", element: <Unauthorized /> },
@@ -43,6 +46,15 @@ const adminRoutes = [
                             { path: "create", element: <SupplierForm /> },
                             { path: "update/:id", element: <SupplierForm /> },
                             { path: "view/:id", element: <SupplierDetails /> },
+                        ],
+                    },
+                    {
+                        path: "product",
+                        children: [
+                            { path: "all", element: <AllProducts /> },
+                            { path: "create", element: <ProductForm /> },
+                            { path: "update/:id", element: <ProductForm /> },
+                            { path: "view/:id", element: <ProductDetails /> },
                         ],
                     },
                 ],
